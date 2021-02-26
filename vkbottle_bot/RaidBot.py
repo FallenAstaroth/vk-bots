64770d9d9b0bd2fb23eabe3f1919fa3b5c66cd6c
Astagnar <47662957+WolfInChains@users.noreply.github.com>
2021-02-26 12:54:29 +0200

    Add files via upload

---
+++ vkbottle_bot/RaidBot.py
@@ -0,0 +1,22 @@
+import time
+import random
+from vkbottle_bot.config import *
+from vkbottle.api import API
+from vkbottle.bot import Bot, Message, run_multibot
+
+bot = Bot()
+
+
+@bot.on.message()
+async def start_flood(message: Message):
+
+    while True:
+
+        sleep = random.uniform(0.199, 0.399)
+
+        await message.answer("", "wall-196079784_129")
+
+        time.sleep(sleep)
+
+
+run_multibot(bot, apis=(API(TOKENS[0]), API(TOKENS[1])))
